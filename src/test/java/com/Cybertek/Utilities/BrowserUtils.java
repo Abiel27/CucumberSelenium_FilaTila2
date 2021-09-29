a61abc1a1ee7f858d510a469c848a1f8f05704b4
Abiel27 <abielmenghistu@gmail.com>
2021-09-29 08:37:04 -0400

    seco

--- src/test/java/com/Cybertek/Utilities/BrowserUtils.java
+++ src/test/java/com/Cybertek/Utilities/BrowserUtils.java
@@ -1,5 +1,19 @@
 package com.Cybertek.Utilities;
 
 public class BrowserUtils {
+    public static void waits(int second){
     //
+
+    /*
+     *waiting for x seconds without handling exception
+     * @param second
+     */
+
+        try {
+        Thread.sleep(second *1000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
 }
+}
+
